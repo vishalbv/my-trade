732ad8273fe5e9fb774d2bc562e8524f74cd16a2
vishalbv <vishalbv23@gmail.com>
2024-11-03 22:59:03 +0530

    added fixes

--- apps/web/app/home/page.tsx
+++ apps/web/app/home/page.tsx
@@ -1,30 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
-import { sendMessage } from "../../src/services/webSocket";
-import { useSelector } from "react-redux";
-import { RootState } from "../../src/store/store";
-
 export default function Home() {
-  return (
-    <div>
-      Home
-      <Test />
-    </div>
-  );
+  return <div>Home</div>;
 }
-
-const Test = () => {
-  const app = useSelector((state: RootState) => state.app || {});
-
-  console.log(app.date, "pppp");
-  useEffect(() => {
-    console.log("Test");
-    setInterval(() => {
-      console.log("sending message");
-      sendMessage("app", { date: new Date().toISOString() });
-    }, 2000);
-  }, []);
-
-  return <div>Test</div>;
-};
